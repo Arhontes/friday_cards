0766caac704386dc0db35754f05ebf3af05a1608
Alex Betke <arhontes@yandex.ru>
2022-08-19 18:20:33 +0300

    add hash router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import {Navigate, Route, Routes } from 'react-router-dom';
+import {HashRouter, Navigate, Route, Routes} from 'react-router-dom';
 import './App.css';
 import {Profile} from "../features/f2-profile/Profile";
 import {Login} from "../features/f1-auth/a1-login/Login";
 import {NewPassword} from "../features/f1-auth/a4-newPassword/NewPassword";
 import {PasswordRecovery} from "../features/f1-auth/a3-passwordRecovery/PasswordRecovery";
-import {Registration} from "../features/f1-auth/a2-registration/Registration";
-import {Error404} from "../features/f3-error/Error404";
 
 function App() {
   return (
-      <Routes>
-        <Route path={"/"} element={<Profile />}/>
-        <Route path={"login"} element={ <Login />}/>
-        <Route path={"registration"} element={ <Registration />}/>
-        <Route path={"/newPassword"} element={<NewPassword />}/>
-        <Route path={"/passwordRecovery"} element={<PasswordRecovery />}/>
-        <Route path={"/404"} element={<Error404 />}/>
-      </Routes>
+      <HashRouter>
+          <Routes>
+              <Route path={"/*"} element={<Profile />}/>
+              <Route path={"/login"} element={ <Login />}/>
+              <Route path={"/newPassword"} element={<NewPassword />}/>
+              <Route path={"/passwordRecovery"} element={<PasswordRecovery />}/>
+              <Route path={"/404"} element={<Navigate to={"/404"}/>}/>
+          </Routes>
+      </HashRouter>
+
   );
 }
 
